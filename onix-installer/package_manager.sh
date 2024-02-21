f80648c1c387ffbf16a9197ff5966c47eebcca8e
PrasadTakale <prasad-takale-eminds>
2024-02-21 20:50:52 +0530

    Fixed bug for accessing variable

--- onix-installer/package_manager.sh
+++ onix-installer/package_manager.sh
@@ -18,8 +18,7 @@ install_package() {
         # APT (Debian/Ubuntu)
         if [ "$1" == "docker" ]; then
             check=$(command_exists docker)
-            if ! check; then
-                echo "Docker"
+            if ! $check; then
                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
                 echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                 sudo apt update >/dev/null 2>&1
