988aff1e458d8992ea39ab0c3690984aca3d9a7f
ShivaniDhoke <shivani.dhoke@eminds.ai>
2024-02-22 20:50:58 +0530

    created status api for industry-4.0-machine-simulator

--- industry-4.0-machine-simulator/src/machine-module/route.ts
+++ industry-4.0-machine-simulator/src/machine-module/route.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
-import { confirmController } from "./controller";
+import { confirmController, getController } from "./controller";
 
 const router: Router = express.Router();
 
 export const machineRoutes = () => {
   router.post("/confirm", confirmController);
+  router.post("/status", getController);
+
   return router;
 };
