3ec803384ff1306d90915cadb93de6c82181ba20
prasad-takale-eminds <prasad.takale@eminds.ai>
2024-03-05 15:51:12 +0530

    Updated script to be compatible with MacOS

--- onix-installer/update_bpp_config.sh
+++ onix-installer/update_bpp_config.sh
@@ -44,7 +44,11 @@ get_keys
 
 
 valid_from=$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")
-valid_until=$(date -u -d "+1 year" +"%Y-%m-%dT%H:%M:%S.%3NZ")
+if [[ $(uname -s ) == 'Darwin' ]];then
+    valid_until=$(date -u -v+1y +"%Y-%m-%dT%H:%M:%S.%3NZ")
+else
+    valid_until=$(date -u -d "+1 year" +"%Y-%m-%dT%H:%M:%S.%3NZ")
+fi
 type=BPP
 
 
@@ -70,7 +74,11 @@ echo "Configuring BPP protocol server"
 # Apply replacements in both files
 for file in "$clientFile" "$networkFile"; do
     for key in "${!replacements[@]}"; do
-        sed -i "s|$key|${replacements[$key]}|" "$file"
+        if [[ $(uname) == "Darwin" ]]; then
+            sed -i '' "s|$key|${replacements[$key]}|" "$file"
+        else
+            sed -i "s|$key|${replacements[$key]}|" "$file"
+        fi
     done
 done
 
